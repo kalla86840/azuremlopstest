abcaee999a9ff57a380a5c3b7a6e61b443c43a73
kalla86840 <122142035+kalla86840@users.noreply.github.com>
2025-04-19 22:27:23 -0700

    Add files via upload

---
+++ prod/scripts/deploy_endpoint.py
@@ -0,0 +1,40 @@
+from azureml.core import Workspace, Model, Environment, InferenceConfig
+from azureml.core.webservice import AciWebservice
+
+ws = Workspace.get(
+    name="mlops test 1",
+    subscription_id="2735eac8-e053-4d04-ad5f-83d5f236217b",
+    resource_group="mlops test 1"
+)
+
+model = Model(ws, "linear_model")
+
+env = Environment.from_conda_specification(
+    name="linear-env",
+    file_path="environment/conda.yml"
+)
+
+inference_config = InferenceConfig(
+    entry_script="scripts/score.py",
+    environment=env
+)
+
+deployment_config = AciWebservice.deploy_configuration(
+    cpu_cores=1,
+    memory_gb=1,
+    auth_enabled=True,
+    tags={"stage": "prod"},
+    description="ACI deployment for linear regression"
+)
+
+service = Model.deploy(
+    workspace=ws,
+    name="linear-endpoint",
+    models=[model],
+    inference_config=inference_config,
+    deployment_config=deployment_config,
+    overwrite=True
+)
+
+service.wait_for_deployment(show_output=True)
+print(f"✅ Endpoint deployed at: {service.scoring_uri}")
