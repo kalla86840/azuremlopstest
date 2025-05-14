20b0d2f743979d15f6ace7679d7acd3e519d974f
kalla86840 <122142035+kalla86840@users.noreply.github.com>
2025-05-14 01:33:00 -0700

    Delete ci directory

--- ci/register_model.py
+++
@@ -1,4 +0,0 @@
-from azureml.core import Workspace, Model
-ws = Workspace.from_config()
-model = Model.register(workspace=ws, model_path='outputs/model.pkl', model_name='car_price_model')
-print("Model registered:", model.name)
